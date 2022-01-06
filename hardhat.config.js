b2b36533155cf4920740b99a0afddf6353cd11ed
Lalit Tanna <lalit@Lalits-MacBook-Pro.local>
2022-01-06 12:24:02 +0000

    Added addresses in cofig
    Added addresses for contract deployment in hardhat.config.js to make deployment abstract from network

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,12 +22,20 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: process.env.MAINNET_HTTP_URL
+      url: process.env.MAINNET_HTTP_URL,
+      UNISWAPV2_ROUTER02: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+      SUSHISWAP_ROUTER: "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+      DYDX_SOLO: "0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e",
+      WETH: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
       }
     },
     kovan: {
       url: process.env.KOVAN_HTTP_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [process.env.PRIVATE_KEY],
+      UNISWAPV2_ROUTER02: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+      SUSHISWAP_ROUTER: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+      DYDX_SOLO: "0x4EC3570cADaAEE08Ae384779B0f3A45EF85289DE",
+      WETH: "0xd0a1e359811322d97991e03f863a0c30c2cf029c"
     }
   },
   solidity: "0.8.4",
