f45283675b1441be88256fb1ff47adc002684f47
lalit <lalittanna77@gmail.com>
2022-01-15 11:54:32 +0000

    Integration test description

--- test/integration/Arbitrage-integration-test.js
+++ test/integration/Arbitrage-integration-test.js
@@ -4,7 +4,6 @@ describe("Arbitrage Integration Test", () => {
   before(async () => {
     const ArbitrageContract = await hre.ethers.getContractFactory("Arbitrage");
 
-    //Add addresses of contracts according to the network
     const arbitrage = await ArbitrageContract.deploy(
       network.config.UNISWAPV2_ROUTER02,
       network.config.SUSHISWAP_ROUTER,
